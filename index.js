73edd29fbe1029c9e090d473371ed11aec41fc70
Faith <thao.faith2@gmail.com>
2021-07-21 06:47:04 -0600

    commit tree d0456e15eed627be0b4ed79b06b92fd6388eb043

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const { arrayContaining } = require("expect")
+
+function findMatching(array, matchName){
+    matchName = matchName.toUpperCase()
+    return array.filter(function(x){return x.toUpperCase() === matchName});
+}
+
+function fuzzyMatch(array, prefix){
+    return array.filter(el => el.startsWith(prefix)) 
+}
+
+function matchName(array, string){
+    return array.filter(el => el.name === string)
+}
